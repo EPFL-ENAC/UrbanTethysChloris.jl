0014bcc1fd5fa8627dc48bcec4854ec0d761bfa6
Hugo Solleder <hugo.solleder@epfl.ch>
2025-05-23 14:18:40 +0200

    refactor: add helper function for canopy_resistance_an_evolution
    Issue(s): #66

--- src/resistance/canopy_resistance_an_evolution.jl
+++ src/resistance/canopy_resistance_an_evolution.jl
@@ -159,133 +159,60 @@ function canopy_resistance_an_evolution(
     PAR_shd = Fshd > 0 ? PAR_shd / (LAI * Fshd) : zero(FT)
 
     # Process sunlit fraction
-    # TODO: refactor by splitting duplicated code in two
-    if Fsun > 0
-        Ci_sun = find_zero(
-            Cc -> co2_concentration(
-                Cc,
-                PAR_sun,
-                Ca,
-                ra,
-                rb_sun,
-                Ts,
-                Pre,
-                Ds,
-                Psi_L,
-                Psi_sto_50,
-                Psi_sto_99,
-                CT,
-                Vmax_sun,
-                DS,
-                Ha,
-                FI,
-                Oa,
-                Do,
-                a1,
-                go_sun,
-                gmes_sun,
-                rjv,
-            ),
-            Citm1_sun;
-            atol=atol,
-        )
-
-        CiF_sun, An_sun, rc_sun, Rdark_sun, SIF_sun = photosynthesis_biochemical(
-            Ci_sun,
-            PAR_sun,
-            Ca,
-            ra,
-            rb_sun,
-            Ts,
-            Pre,
-            Ds,
-            Psi_L,
-            Psi_sto_50,
-            Psi_sto_99,
-            CT,
-            Vmax_sun,
-            DS,
-            Ha,
-            FI,
-            Oa,
-            Do,
-            a1,
-            go_sun,
-            gmes_sun,
-            rjv,
-        )
-    else
-        Ci_sun = zero(FT)
-        CiF_sun = zero(FT)
-        An_sun = zero(FT)
-        Rdark_sun = zero(FT)
-        rc_sun = Inf
-        SIF_sun = zero(FT)
-    end
+    Ci_sun, CiF_sun, An_sun, rc_sun, Rdark_sun, SIF_sun = process_leaf_fraction(
+        Fsun,
+        PAR_sun,
+        Citm1_sun,
+        Ca,
+        ra,
+        rb_sun,
+        Ts,
+        Pre,
+        Ds,
+        Psi_L,
+        Psi_sto_50,
+        Psi_sto_99,
+        CT,
+        Vmax_sun,
+        DS,
+        Ha,
+        FI,
+        Oa,
+        Do,
+        a1,
+        go_sun,
+        gmes_sun,
+        rjv,
+        atol,
+    )
 
     # Process shaded fraction
-    if Fshd > 0
-        Ci_shd = find_zero(
-            Cc -> co2_concentration(
-                Cc,
-                PAR_shd,
-                Ca,
-                ra,
-                rb_shd,
-                Ts,
-                Pre,
-                Ds,
-                Psi_L,
-                Psi_sto_50,
-                Psi_sto_99,
-                CT,
-                Vmax_shd,
-                DS,
-                Ha,
-                FI,
-                Oa,
-                Do,
-                a1,
-                go_shd,
-                gmes_shd,
-                rjv,
-            ),
-            Citm1_shd;
-            atol=atol,
-        )
-
-        CiF_shd, An_shd, rc_shd, Rdark_shd, SIF_shd = photosynthesis_biochemical(
-            Ci_shd,
-            PAR_shd,
-            Ca,
-            ra,
-            rb_shd,
-            Ts,
-            Pre,
-            Ds,
-            Psi_L,
-            Psi_sto_50,
-            Psi_sto_99,
-            CT,
-            Vmax_shd,
-            DS,
-            Ha,
-            FI,
-            Oa,
-            Do,
-            a1,
-            go_shd,
-            gmes_shd,
-            rjv,
-        )
-    else
-        Ci_shd = zero(FT)
-        CiF_shd = zero(FT)
-        An_shd = zero(FT)
-        Rdark_shd = zero(FT)
-        rc_shd = Inf
-        SIF_shd = zero(FT)
-    end
+    Ci_shd, CiF_shd, An_shd, rc_shd, Rdark_shd, SIF_shd = process_leaf_fraction(
+        Fshd,
+        PAR_shd,
+        Citm1_shd,
+        Ca,
+        ra,
+        rb_shd,
+        Ts,
+        Pre,
+        Ds,
+        Psi_L,
+        Psi_sto_50,
+        Psi_sto_99,
+        CT,
+        Vmax_shd,
+        DS,
+        Ha,
+        FI,
+        Oa,
+        Do,
+        a1,
+        go_shd,
+        gmes_shd,
+        rjv,
+        atol,
+    )
 
     # Calculate CO₂ concentration changes
     DCi_sun = Ci_sun - CiF_sun
@@ -305,4 +232,160 @@ function canopy_resistance_an_evolution(
     Lpho = (An + Rdark) * lanp  # [W/m²]
 
     return rs_sun, rs_shd, Ci_sun, Ci_shd, An, Rdark, Lpho, SIF, DCi
+end
+
+"""
+    process_leaf_fraction(
+        F::FT,
+        PAR::FT,
+        Citm1::FT,
+        Ca::FT,
+        ra::FT,
+        rb::FT,
+        Ts::FT,
+        Pre::FT,
+        Ds::FT,
+        Psi_L::FT,
+        Psi_sto_50::FT,
+        Psi_sto_99::FT,
+        CT::Int,
+        Vmax::FT,
+        DS::FT,
+        Ha::FT,
+        FI::FT,
+        Oa::FT,
+        Do::FT,
+        a1::FT,
+        go::FT,
+        gmes::FT,
+        rjv::FT,
+        atol::FT
+    ) where {FT<:AbstractFloat}
+
+Process CO₂ assimilation for a leaf fraction (sunlit or shaded).
+
+# Arguments
+- `F`: Leaf fraction [-]
+- `PAR`: Photosynthetically active radiation [W/m²]
+- `Citm1`: Previous CO₂ concentration [μmolCO₂/mol]
+- `Ca`: Atmospheric CO₂ concentration [μmolCO₂/mol]
+- `ra`: Aerodynamic resistance [s/m]
+- `rb`: Boundary layer resistance [s/m]
+- `Ts`: Leaf temperature [K]
+- `Pre`: Atmospheric pressure [Pa]
+- `Ds`: Vapor pressure deficit [Pa]
+- `Psi_L`: Leaf water potential [MPa]
+- `Psi_sto_50`: 50% storage water potential [MPa]
+- `Psi_sto_99`: 99% storage water potential [MPa]
+- `CT`: Temperature coefficient
+- `Vmax`: Maximum carboxylation rate [μmolCO₂/m²/s]
+- `DS`: Stomatal conductance [m/s]
+- `Ha`: Leaf area [m²]
+- `FI`: Light intensity [μmol photons/m²/s]
+- `Oa`: Ambient oxygen concentration [μmolO₂/mol]
+- `Do`: Diffusion coefficient [m²/s]
+- `a1`: Parameter for photosynthesis model
+- `go`: Stomatal conductance [m/s]
+- `gmes`: Mesophyll conductance [m/s]
+- `rjv`: Jmax/Rubisco parameter
+- `atol`: Absolute tolerance for root finding
+
+# Returns
+- `Ci`: Leaf internal CO₂ concentration [μmolCO₂/mol]
+- `CiF`: Leaf internal CO₂ concentration at equilibrium [μmolCO₂/mol]
+- `An`: Net assimilation rate [μmolCO₂/m²/s]
+- `rc`: Canopy resistance [s/m]
+- `Rdark`: Dark respiration rate [μmolCO₂/m²/s]
+- `SIF`: Solar-induced fluorescence [W/m²/sr/μm]
+"""
+function process_leaf_fraction(
+    F::FT,
+    PAR::FT,
+    Citm1::FT,
+    Ca::FT,
+    ra::FT,
+    rb::FT,
+    Ts::FT,
+    Pre::FT,
+    Ds::FT,
+    Psi_L::FT,
+    Psi_sto_50::FT,
+    Psi_sto_99::FT,
+    CT::Int,
+    Vmax::FT,
+    DS::FT,
+    Ha::FT,
+    FI::FT,
+    Oa::FT,
+    Do::FT,
+    a1::FT,
+    go::FT,
+    gmes::FT,
+    rjv::FT,
+    atol::FT,
+) where {FT<:AbstractFloat}
+    if F > 0
+        Ci = find_zero(
+            Cc -> co2_concentration(
+                Cc,
+                PAR,
+                Ca,
+                ra,
+                rb,
+                Ts,
+                Pre,
+                Ds,
+                Psi_L,
+                Psi_sto_50,
+                Psi_sto_99,
+                CT,
+                Vmax,
+                DS,
+                Ha,
+                FI,
+                Oa,
+                Do,
+                a1,
+                go,
+                gmes,
+                rjv,
+            ),
+            Citm1;
+            atol=atol,
+        )
+
+        CiF, An, rc, Rdark, SIF = photosynthesis_biochemical(
+            Ci,
+            PAR,
+            Ca,
+            ra,
+            rb,
+            Ts,
+            Pre,
+            Ds,
+            Psi_L,
+            Psi_sto_50,
+            Psi_sto_99,
+            CT,
+            Vmax,
+            DS,
+            Ha,
+            FI,
+            Oa,
+            Do,
+            a1,
+            go,
+            gmes,
+            rjv,
+        )
+    else
+        Ci = zero(FT)
+        CiF = zero(FT)
+        An = zero(FT)
+        rc = Inf
+        Rdark = zero(FT)
+        SIF = zero(FT)
+    end
+
+    return Ci, CiF, An, rc, Rdark, SIF
 end
