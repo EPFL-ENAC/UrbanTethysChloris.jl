ff8d826eaaf1a1492ebc381b3a324fec42d69bbb
Hugo Solleder <hugo.solleder@epfl.ch>
2025-06-06 14:02:04 +0200

    fix: correct 4x error in photosynthesis_biochemical

--- test/resistance/photosynthesis_biochemical.jl
+++ test/resistance/photosynthesis_biochemical.jl
@@ -61,3 +61,56 @@ FT = Float64
     @test GAM ≈ 1.268521967198623
     @test gsCO2 ≈ 10000
 end
+
+@testset "TethysChloris.jl issue #136" begin
+    CT = 3
+    Cc = 200.0
+    Csl = 338.43
+    DS = 0.656
+    Do = 1000.0
+    Ds = 310.5037657846657
+    FI = 0.081
+    Ha = 55.0
+    IPAR = 1.602272776775221
+    Oa = 210000.0
+    Pre = 948.7232105699336
+    Psi_L = -0.019349689699981
+    Psi_sto_00 = -0.5
+    Psi_sto_50 = -3.0
+    Ta = 4.1
+    Ts = 4.1
+    Vmax = 83.571947364501554
+    a1 = 7.0
+    gmes = Inf
+    go = 0.01
+    ra = 20.725120860990302
+    rb = 15.231030461445188
+    rjv = 2.4
+
+    CcF, An, rs, Rdark, F755nm, GAM, gsCO2 = photosynthesis_biochemical(
+        Cc,
+        IPAR,
+        Csl,
+        ra,
+        rb,
+        Ts,
+        Pre,
+        Ds,
+        Psi_L,
+        Psi_sto_50,
+        Psi_sto_00,
+        CT,
+        Vmax,
+        DS,
+        Ha,
+        FI,
+        Oa,
+        Do,
+        a1,
+        go,
+        gmes,
+        rjv,
+    )
+
+    @test CcF ≈ 329.4521734704098
+end
