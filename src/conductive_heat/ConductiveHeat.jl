b8c40b04a0f9c929615813f4d939ce87dca895de
Hugo Solleder <hugo.solleder@epfl.ch>
2025-06-23 08:00:14 +0200

    chore: remove conductive heat exports
    Issue(s): #75

--- src/conductive_heat/ConductiveHeat.jl
+++ src/conductive_heat/ConductiveHeat.jl
@@ -4,8 +4,6 @@ using ..ModelComponents
 using ..Soil: soil_parameters_total, soil_thermal_properties
 using LinearAlgebra: dot
 
-export conductive_heat_flux_ground_vb
-
 include("conductive_heat_flux_green_roof.jl")
 include("conductive_heat_flux_roof_imp.jl")
 include("conductive_heat_flux_walls.jl")
