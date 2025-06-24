289ad158e27b4e4e73dab77d4981e18207e2891b
Hugo Solleder <hugo.solleder@epfl.ch>
2025-06-24 16:23:32 +0200

    chore: translate SWRDiffPerson
    Issue(s): #81

---
+++ src/mean_radiant_temperature/MeanRadiantTemperature.jl
@@ -0,0 +1,7 @@
+module MeanRadiantTemperature
+
+using ..RayTracing: ViewFactorPoint
+
+include("swr_diff_person.jl")
+
+end
---
+++ test/translation/julia/mean_radiant_temperature/MeanRadiantTemperature.jl
@@ -0,0 +1,5 @@
+using SafeTestsets
+
+@safetestset "SWRDiffPerson" begin
+    include("swr_diff_person.jl")
+end
