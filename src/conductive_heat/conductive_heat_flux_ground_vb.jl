9cba86acea2819a975e0c1631ce3a229a99bd09d
Hugo Solleder <hugo.solleder@epfl.ch>
2025-12-02 21:57:00 +0100

    feat: switch f_solver_tot subfunctions to composite types

--- src/conductive_heat/conductive_heat_flux_ground_vb.jl
+++ src/conductive_heat/conductive_heat_flux_ground_vb.jl
@@ -32,6 +32,87 @@ Calculate conductive heat flux for bare/vegetated ground fractions.
 """
 function conductive_heat_flux_ground_vb(
     TemperatureC::Vector{FT},
+    TempDamp_ittm::ModelComponents.ModelVariables.TempDamp{FT},
+    Owater_ittm::ModelComponents.ModelVariables.Owater{FT,MR,MG},
+    TempVec_ittm::ModelComponents.ModelVariables.TempVec{FT},
+    ParCalculation::NamedTuple,
+    ParSoilGround::ModelComponents.Parameters.VegetatedSoilParameters{FT},
+    ParVegGround::ModelComponents.Parameters.HeightDependentVegetationParameters{FT},
+    ParVegTree::ModelComponents.Parameters.HeightDependentVegetationParameters{FT},
+    FractionsGround::ModelComponents.Parameters.LocationSpecificSurfaceFractions{FT},
+    type::Int,
+) where {FT<:AbstractFloat,MR,MG}
+    if type == 0  # bare soil
+        Ts = TemperatureC[2]
+        Tdptm1 = TempDamp_ittm.TDampGroundBare
+        Otm1 = Owater_ittm.OwGroundSoilBare
+        Tstm1 = TempVec_ittm.TGroundBare
+        Csoil = FractionsGround.fbare > 0
+    elseif type == 1  # vegetated ground
+        Ts = TemperatureC[3]
+        Tdptm1 = TempDamp_ittm.TDampGroundVeg
+        Otm1 = Owater_ittm.OwGroundSoilVeg
+        Tstm1 = TempVec_ittm.TGroundVeg
+        Csoil = FractionsGround.fveg > 0
+    else
+        throw(
+            ArgumentError(
+                "Please enter a valid specification of ground type. 0 = bare, 1 = vegetated"
+            ),
+        )
+    end
+
+    dts = ParCalculation.dts
+    Pcla = ParSoilGround.Pcla
+    Psan = ParSoilGround.Psan
+    Porg = ParSoilGround.Porg
+    Kfc = ParSoilGround.Kfc
+    Phy = ParSoilGround.Phy
+    SPAR = ParSoilGround.SPAR
+    Kbot = ParSoilGround.Kbot
+    CASE_ROOT_H = ParVegTree.CASE_ROOT
+    CASE_ROOT_L = ParVegGround.CASE_ROOT
+    ZR95_H = ParVegTree.ZR95
+    ZR95_L = ParVegGround.ZR95
+    ZR50_H = ParVegTree.ZR50
+    ZR50_L = ParVegGround.ZR50
+    ZRmax_H = ParVegTree.ZRmax
+    ZRmax_L = ParVegGround.ZRmax
+    Zs = ParSoilGround.Zs
+
+    _, dz, _, Osat, Ohy, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, rsd, lan_dry, lan_s, cv_s = soil_parameters_total(
+        Pcla,
+        Psan,
+        Porg,
+        Kfc,
+        Phy,
+        SPAR,
+        Kbot,
+        CASE_ROOT_H,
+        CASE_ROOT_L,
+        ZR95_H,
+        ZR95_L,
+        ZR50_H,
+        ZR50_L,
+        ZRmax_H,
+        ZRmax_L,
+        Zs,
+    )
+
+    Otm1Ave = sum((dz ./ sum(dz)) .* Otm1)
+
+    _, _, CTt = soil_thermal_properties(
+        Tdptm1 - 273.15, rsd, lan_dry, lan_s, cv_s, Osat, Ohy, [Otm1Ave]
+    )
+
+    G, Tdp = soil_heat(dts, Ts - 273.15, Tstm1 - 273.15, Tdptm1 - 273.15, CTt[1])
+    Tdp = Tdp + 273.15
+    G = G * Csoil
+
+    return G, Tdp
+end
+function conductive_heat_flux_ground_vb(
+    TemperatureC::Vector{FT},
     TempDamp_ittm::NamedTuple,
     Owater_ittm::NamedTuple,
     TempVec_ittm::NamedTuple,
--- src/turbulent_heat/heat_flux_wall.jl
+++ src/turbulent_heat/heat_flux_wall.jl
@@ -44,6 +44,179 @@ Calculate sensible and latent heat fluxes for sunlit and shaded walls.
 function heat_flux_wall(
     TemperatureC::AbstractVector{FT},
     Gemeotry_m::ModelComponents.Parameters.UrbanGeometryParameters{FT},
+    MeteoData::ModelComponents.ForcingInputs.MeteorologicalInputs{FT,0},
+    ParVegTree::ModelComponents.Parameters.HeightDependentVegetationParameters{FT},
+    ParVegGround::ModelComponents.Parameters.HeightDependentVegetationParameters{FT},
+    FractionsGround::ModelComponents.Parameters.LocationSpecificSurfaceFractions{FT},
+) where {FT<:AbstractFloat}
+    # Extract temperatures
+    Timp = TemperatureC[1]
+    Tbare = TemperatureC[2]
+    Tveg = TemperatureC[3]
+    Ttree = TemperatureC[6]
+    Twsun = TemperatureC[4]
+    Twshade = TemperatureC[5]
+    T_canyon = TemperatureC[9]
+    qcanyon = TemperatureC[10]
+
+    # Extract geometry parameters
+    H = Gemeotry_m.Height_canyon
+    W = Gemeotry_m.Width_canyon
+    Wroof = Gemeotry_m.Width_roof
+    Htree = Gemeotry_m.Height_tree
+    R_tree = Gemeotry_m.Radius_tree
+    Hcan_max = Gemeotry_m.Hcan_max
+    Hcan_std = Gemeotry_m.Hcan_std
+    rad_tree = R_tree / W
+
+    # Extract vegetation parameters
+    Kopt = ParVegTree.Kopt
+    LAI_t = ParVegTree.LAI
+    trees = Gemeotry_m.trees
+    hc_L = ParVegGround.hc
+
+    # Extract meteorological data
+    Zatm = MeteoData.Zatm
+    Tatm = MeteoData.Tatm
+    Uatm = MeteoData.Uatm
+    Pre = MeteoData.Pre
+    ea = MeteoData.ea
+
+    # Calculate thermodynamic properties
+    cp_atm = 1005 + ((Tatm - 273.15) + 23.15)^2 / 3364
+    rho_atm = (Pre / (287.04 * Tatm)) * (1 - (ea / Pre) * (1 - 0.622))
+    L_heat = 1000 * (2501.3 - 2.361 * (Tatm - 273.15))
+
+    # Vapor pressure calculation
+    e_T_canyon = qcanyon * Pre / (0.622 + 0.378 * qcanyon)
+
+    # Average surface temperature calculation
+    Ctree = FT(trees)
+    Tsurf =
+        (
+            FractionsGround.fveg * Tveg +
+            FractionsGround.fbare * Tbare +
+            FractionsGround.fimp * Timp +
+            Ctree * Ttree * (4 * rad_tree) +
+            H/W * Twsun +
+            H/W * Twshade
+        ) / (
+            FractionsGround.fveg +
+            FractionsGround.fbare +
+            FractionsGround.fimp +
+            (Ctree * 4 * rad_tree) +
+            2 * H/W
+        )
+
+    # Calculate roughness parameters
+    roughness = urban_roughness(
+        zero(FT),
+        FT(FractionsGround.fveg > 0) * hc_L,
+        FractionsGround.fbare > 0,
+        FractionsGround.fimp > 0,
+        false,
+    )
+    zom_ground = roughness[3]
+
+    # Calculate wind profile parameters
+    wind_params = wind_profile_canyon(
+        H,
+        Htree,
+        R_tree,
+        W,
+        Wroof,
+        Kopt,
+        LAI_t,
+        Zatm,
+        Uatm,
+        FT(2),
+        trees,
+        FT(1.5),
+        zom_ground,
+        Hcan_max,
+        Hcan_std,
+    )
+    dcan = wind_params[1]
+    zomcan = wind_params[2]
+    RoughnessParameter = wind_params[7]
+
+    # Define calculation heights
+    Zp1 = FT(2)
+    Zp2 = 2 * Zp1 + (H - 2 * Zp1) / 2
+    wcan = zero(FT)
+
+    # Calculate in-canyon aerodynamic resistances
+    canyon_res = in_canyon_aerodynamic_resistance(
+        Uatm,
+        Zatm,
+        T_canyon-273.15,
+        Tsurf-273.15,
+        Hcan_max,
+        H,
+        dcan,
+        zomcan,
+        FT(1.5),
+        zom_ground,
+        Zp1,
+        Zp2,
+        FT(2),
+        Pre,
+        e_T_canyon,
+        RoughnessParameter,
+    )
+    rap_Zp1 = canyon_res[2]
+    rap_Zp1_In = canyon_res[3]
+    rap_Zp2 = canyon_res[4]
+    rap_Zp2_In = canyon_res[5]
+    u_Zp1 = canyon_res[9]
+    u_Zp2 = canyon_res[10]
+
+    # Calculate wall resistances
+    RES_w1 = cp_atm * rho_atm / (11.8 + 4.2 * sqrt(u_Zp1^2 + wcan^2))
+    RES_w2 = cp_atm * rho_atm / (11.8 + 4.2 * sqrt(u_Zp2^2 + wcan^2))
+
+    # Calculate heat fluxes for each level
+    Hwsun1 = cp_atm * rho_atm * (Twsun - T_canyon) / (RES_w1 + rap_Zp1_In)
+    Hwshade1 = cp_atm * rho_atm * (Twshade - T_canyon) / (RES_w1 + rap_Zp1_In)
+
+    Hwsun2 = cp_atm * rho_atm * (Twsun - T_canyon) / (RES_w2 + rap_Zp2_In)
+    Hwshade2 = cp_atm * rho_atm * (Twshade - T_canyon) / (RES_w2 + rap_Zp2_In)
+
+    # Combine fluxes
+    Hwsun = min(2 * Zp1/H, 1) * Hwsun1 + max((H - 2 * Zp1)/H, 0) * Hwsun2
+    Hwshade = min(2 * Zp1/H, 1) * Hwshade1 + max((H - 2 * Zp1)/H, 0) * Hwshade2
+
+    # No latent heat for walls
+    Ewsun = zero(FT)
+    Ewshade = zero(FT)
+    LEwsun = L_heat * Ewsun
+    LEwshade = L_heat * Ewshade
+
+    return Hwsun,
+    Hwshade,
+    Ewsun,
+    Ewshade,
+    LEwsun,
+    LEwshade,
+    RES_w1,
+    RES_w2,
+    rap_Zp1_In,
+    rap_Zp2_In,
+    Hwsun1,
+    Hwshade1,
+    Hwsun2,
+    Hwshade2,
+    cp_atm,
+    rho_atm,
+    L_heat,
+    Zp1,
+    Zp2,
+    rap_Zp1,
+    rap_Zp2
+end
+function heat_flux_wall(
+    TemperatureC::AbstractVector{FT},
+    Gemeotry_m::ModelComponents.Parameters.UrbanGeometryParameters{FT},
     MeteoData::NamedTuple,
     ParVegTree::ModelComponents.Parameters.HeightDependentVegetationParameters{FT},
     ParVegGround::ModelComponents.Parameters.HeightDependentVegetationParameters{FT},
